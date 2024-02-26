de31823b041af935e62401a585b5ca1feca4f216
seunghoona <vicjed@icloud.com>
2024-02-26 19:45:29 +0900

    add loginForm

--- lib/screens/authentication/login_form_screen.dart
+++ lib/screens/authentication/login_form_screen.dart
@@ -1,10 +1,78 @@
-import 'package:flutter/material.dart';
+import 'dart:collection';
 
-class LoginFormScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/authentication/widgets/form_button.dart';
+
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Map<String, String> formData = HashMap();
+
+  void _onSubmitTap() {
+    if (_formKey.currentState?.validate() ?? false) {
+      _formKey.currentState!.save();
+      print(formData);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Gaps.v28,
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "email",
+                  ),
+                  validator: (value) {
+                    return null;
+                    // "invalid email";
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData["email"] = newValue;
+                    }
+                  },
+                ),
+                Gaps.v10,
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "password",
+                  ),
+                  validator: (value) {
+                    return null;
+                    // "wrong password";
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData["password"] = newValue;
+                    }
+                  },
+                ),
+                Gaps.v28,
+                GestureDetector(
+                    onTap: _onSubmitTap,
+                    child: const FormButton(disabled: false))
+              ],
+            )),
+      ),
+    );
   }
 }
--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/authentication/login_form_screen.dart';
 
 import 'widgets/auth_button.dart';
 
@@ -12,16 +13,24 @@ class LoginScreen extends StatelessWidget {
     Navigator.of(context).pop();
   }
 
+  void _onLoginFormTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const LoginFormScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const SafeArea(
+      body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: Sizes.size32),
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
           child: Column(
             children: [
               Gaps.v80,
-              Text(
+              const Text(
                 "Log in to TikTok",
                 style: TextStyle(
                   fontSize: Sizes.size24,
@@ -29,15 +38,18 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
               Gaps.v14,
-              Text(
+              const Text(
                   "Manage your account, check notifications, comment on videos, and more."),
               Gaps.v40,
-              AuthButton(
-                text: "Use email or password",
-                faIcon: FaIcon(FontAwesomeIcons.user),
+              GestureDetector(
+                onTap: () => _onLoginFormTap(context),
+                child: const AuthButton(
+                  text: "Use email or password",
+                  faIcon: FaIcon(FontAwesomeIcons.user),
+                ),
               ),
               Gaps.v16,
-              AuthButton(
+              const AuthButton(
                 text: "Contunue with Apple",
                 faIcon: FaIcon(FontAwesomeIcons.apple),
               ),
