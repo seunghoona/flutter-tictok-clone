e55c0cd276ad48e2d2f2d7746efc048a641e5017
seunghoona <vicjed@icloud.com>
2024-03-05 20:42:04 +0900

    Add comment inputbox action

--- lib/screens/vidoes/widget/video_comment.dart
+++ lib/screens/vidoes/widget/video_comment.dart
@@ -11,8 +11,25 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  bool _isWriting = false;
+
+  final ScrollController _scrollerController = ScrollController();
+
   void _onClosePressed() {
     Navigator.of(context).pop();
+  }
+
+  void _startWriting() {
+    setState(() {
+      _isWriting = true;
+    });
+  }
+
+  void _stopWriting() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWriting = false;
+    });
   }
 
   @override
@@ -37,52 +54,59 @@ class _VideoCommentsState extends State<VideoComments> {
         ),
         body: Stack(
           children: [
-            ListView.separated(
-              itemCount: 10,
-              separatorBuilder: (context, index) => Gaps.v10,
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size16,
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CircleAvatar(
-                      child: Text("승후"),
+            Scrollbar(
+              controller: _scrollerController,
+              child: ListView.separated(
+                controller: _scrollerController,
+                itemCount: 10,
+                separatorBuilder: (context, index) => Gaps.v10,
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: _stopWriting,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size16,
                     ),
-                    Gaps.h10,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "승후",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: Sizes.size14,
-                              color: Colors.grey.shade500,
-                            ),
-                          ),
-                          const Text("영어 회화 독학을 위한 생활 영어 표현 연속듣기 시리즈! "),
-                        ],
-                      ),
-                    ),
-                    Gaps.h10,
-                    Column(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        FaIcon(
-                          color: Colors.grey.shade500,
-                          FontAwesomeIcons.heart,
+                        const CircleAvatar(
+                          child: Text("승후"),
                         ),
-                        Text(
-                          "22 K",
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
+                        Gaps.h10,
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "승후",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Sizes.size14,
+                                  color: Colors.grey.shade500,
+                                ),
+                              ),
+                              const Text("영어 회화 독학을 위한 생활 영어 표현 연속듣기 시리즈! "),
+                            ],
                           ),
+                        ),
+                        Gaps.h10,
+                        Column(
+                          children: [
+                            FaIcon(
+                              color: Colors.grey.shade500,
+                              FontAwesomeIcons.heart,
+                            ),
+                            Text(
+                              "22 K",
+                              style: TextStyle(
+                                color: Colors.grey.shade500,
+                              ),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -102,20 +126,62 @@ class _VideoCommentsState extends State<VideoComments> {
                     ),
                     Gaps.h10,
                     Expanded(
-                      child: TextField(
-                        cursorColor: Theme.of(context).primaryColor,
-                        decoration: InputDecoration(
-                          hintText: "Add Comment",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(Sizes.size12),
-                            borderSide: BorderSide.none,
-                          ),
-                          fillColor: Colors.grey.shade300,
-                          filled: true,
-                          contentPadding: const EdgeInsets.symmetric(
-                            vertical: Sizes.size10,
-                            horizontal: Sizes.size10,
-                          ),
+                      child: SizedBox(
+                        height: Sizes.size40,
+                        child: TextField(
+                          onTap: _startWriting,
+                          expands: true,
+                          minLines: null,
+                          maxLines: null,
+                          textInputAction: TextInputAction.newline,
+                          cursorColor: Theme.of(context).primaryColor,
+                          decoration: InputDecoration(
+                              hintText: "Add Comment",
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                                borderSide: BorderSide.none,
+                              ),
+                              fillColor: Colors.grey.shade300,
+                              filled: true,
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: Sizes.size10,
+                              ),
+                              suffixIcon: Padding(
+                                padding: const EdgeInsets.only(right: 10),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const FaIcon(
+                                      FontAwesomeIcons.at,
+                                      size: Sizes.size20,
+                                      color: Colors.black,
+                                    ),
+                                    Gaps.h6,
+                                    const FaIcon(
+                                      FontAwesomeIcons.gift,
+                                      size: Sizes.size20,
+                                      color: Colors.black,
+                                    ),
+                                    Gaps.h6,
+                                    const FaIcon(
+                                      FontAwesomeIcons.faceSmile,
+                                      size: Sizes.size20,
+                                      color: Colors.black,
+                                    ),
+                                    Gaps.h6,
+                                    if (_isWriting)
+                                      GestureDetector(
+                                          onTap: _stopWriting,
+                                          child: FaIcon(
+                                            FontAwesomeIcons.circleArrowUp,
+                                            size: Sizes.size20,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                          ))
+                                  ],
+                                ),
+                              )),
                         ),
                       ),
                     )
